7e993880f14ecf265004ded1d525eb426813e796
Kam D Kasravi <kam.d.kasravi@intel.com>
2019-10-24 18:37:53 -0700

    update images

--- tpu/run-resnet.sh
+++ tpu/run-resnet.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
+if [[ -z $PYENV_ROOT ]]; then
+  source $HOME/.profile
+fi
 pyenv activate tpu
 python resnet/resnet_main.py --data_dir=gs://cloud-tpu-test-datasets/fake_imagenet --model_dir=gs://kdkasrav/resnet
