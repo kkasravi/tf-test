c7ebaa5a21df46904e47c9868b59226c7d267a7f
Kam D Kasravi <kam.d.kasravi@intel.com>
2019-11-19 10:10:53 -0800

    updates to gpu

---
+++ gpu/env-model.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+if [[ -z $PYENV_ROOT ]]; then
+  source $HOME/.profile
+fi
+pyenv install 3.6.8
+pyenv virtualenv 3.6.8 tpu
+pyenv local tpu
+pyenv global tpu
+pip install pipenv
+pipenv install absl-py
+pipenv install pyyaml
+pipenv install wheel
+pipenv install tensorflow==1.14
+pipenv install google-cloud-storage
+pipenv install google-api-python-client
+pipenv install oauth2client
