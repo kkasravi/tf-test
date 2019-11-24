af93e415aa14534dcf13a3ddfbeaf2233f1e150b
Kam D Kasravi <kam.d.kasravi@intel.com>
2019-11-24 08:13:20 -0800

    update gpu/simple-model to save a simple-model.pbtxt

--- gpu/simple-model.py
+++ gpu/simple-model.py
@@ -7,10 +7,9 @@ from packaging import version
 
 import tensorflow as tf
 from tensorflow import keras
-from tensorflow import hooks
 
 print("TensorFlow version: ", tf.__version__)
-assert version.parse(tf.__version__).release[0] >= 2, \
+assert version.parse(tf.__version__).release[0] >= 1 and version.parse(tf.__version__).release[1] >= 14, \
     "Requires TensorFlow 2.0 or above."
 
 
@@ -37,8 +36,6 @@ logdir="logs/fit/" + datetime.now().strftime("%Y%m%d-%H%M%S")
 tensorboard_callback = keras.callbacks.TensorBoard(log_dir=logdir)
 
 # Train the model.
-profiler_hook = hooks.ProfilerHook(save_steps=10, output_dir=args.job_dir)
-
 model.fit(
     train_images,
     train_labels, 
@@ -46,4 +43,37 @@ model.fit(
     epochs=5, 
     callbacks=[tensorboard_callback])
 
-model.save('/kubeflow/logs/saved_model', save_format='tf')
+def freeze_session(session, keep_var_names=None, output_names=None, clear_devices=True):
+    """
+    Freezes the state of a session into a pruned computation graph.
+
+    Creates a new computation graph where variable nodes are replaced by
+    constants taking their current value in the session. The new graph will be
+    pruned so subgraphs that are not necessary to compute the requested
+    outputs are removed.
+    @param session The TensorFlow session to be frozen.
+    @param keep_var_names A list of variable names that should not be frozen,
+                          or None to freeze all the variables in the graph.
+    @param output_names Names of the relevant graph outputs.
+    @param clear_devices Remove the device directives from the graph for better portability.
+    @return The frozen graph definition.
+    """
+    graph = session.graph
+    with graph.as_default():
+        freeze_var_names = list(set(v.op.name for v in tf.global_variables()).difference(keep_var_names or []))
+        output_names = output_names or []
+        output_names += [v.op.name for v in tf.global_variables()]
+        input_graph_def = graph.as_graph_def()
+        if clear_devices:
+            for node in input_graph_def.node:
+                node.device = ''
+        frozen_graph = tf.graph_util.convert_variables_to_constants(
+            session, input_graph_def, output_names, freeze_var_names)
+        return frozen_graph
+
+model.save('./logs/saved_model', save_format='h5')
+model.summary()
+
+frozen_graph = freeze_session(tf.keras.backend.get_session(), output_names=[out.op.name for out in model.outputs])
+tf.train.write_graph(frozen_graph, './logs', 'simple-model.pbtxt', as_text=True)
+
